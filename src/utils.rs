fe41e9814dd824b716c950b756bf769cf14acd57
Jake Howard <git@theorangeone.net>
2020-12-07 21:49:11 +0000

    Remove unnecessary returns

--- src/utils.rs
+++ src/utils.rs
@@ -1,17 +1,17 @@
 use std::env;
 
 pub fn get_port() -> u16 {
-    return env::var("PORT")
+    env::var("PORT")
         .unwrap_or_else(|_| "5000".into())
         .parse::<u16>()
-        .expect("Invalid port number");
+        .expect("Invalid port number")
 }
 
 pub fn get_workers() -> usize {
-    return env::var("WORKERS")
+    env::var("WORKERS")
         .unwrap_or_else(|_| "1".into())
         .parse::<usize>()
-        .expect("Invalid worker count");
+        .expect("Invalid worker count")
 }
 
 #[cfg(test)]
